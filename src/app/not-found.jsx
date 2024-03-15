833df607afb046164017a5a88b563aee02a87c37
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-15 22:18:49 +0530

    added not-found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div>
+      <div>NotFound</div>
+      <Link href="/">return home</Link>
+    </div>
+  );
+};
+
+export default NotFound;
