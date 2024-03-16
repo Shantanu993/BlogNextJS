71441841efcae637ab413464351876011c57c65b
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-16 18:20:38 +0530

    create homepage

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Links from "./links/Links";
 import styles from "./navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div>logo</div>
+      <Link href="/" className={styles.logo}>
+        logo
+      </Link>
       <div>
         <Links />
       </div>
