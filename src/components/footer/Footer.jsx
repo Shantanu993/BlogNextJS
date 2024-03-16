f4f8bb8abce4db231f763ec95d37f093a064a7f6
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-16 18:23:47 +0530

    create footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,12 @@
-import React from "react";
 import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>SG993</div>
+      <div className={styles.text}>SG993 © All rights reserved.</div>
+    </div>
+  );
 };
 
 export default Footer;
