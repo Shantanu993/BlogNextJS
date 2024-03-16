add8e4f7e6ff1fe63f74392e0728383145e8910d
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-16 09:14:05 +0530

    Add active class to NavLink based on current pathname

--- src/components/navbar/links/navLink/navLInk.jsx
+++ src/components/navbar/links/navLink/navLInk.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "./navLink.module.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavLInk = ({ item }) => {
+  const pathName = usePathname();
   return (
-    <Link href={item.path} key={item.path} className={styles.navLink}>
+    <Link
+      href={item.path}
+      key={item.path}
+      className={`${styles.navLink} ${
+        pathName === item.title && styles.active
+      }`}
+    >
       {item.title}
     </Link>
   );
