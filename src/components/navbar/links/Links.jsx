93aa853b1aaaacc47d1fb9b1e1875ece198604f8
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-16 18:01:39 +0530

    Refactor navbar links component to include mobile menu

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,44 +1,60 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import NavLInk from "./navLink/navLInk";
 import styles from "./links.module.css";
 
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
 const Links = () => {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+  const [open, setOpen] = useState(false);
 
   // TEMPORARY
   const session = true;
   const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLInk item={link} key={link.title} />
-      ))}
-      {session ? (
-        <>
-          {isAdmin && <NavLInk item={{ title: "Admin", path: "/admin" }} />}
-          <button>Logout</button>
-        </>
-      ) : (
-        <NavLInk item={{ title: "Login", path: "/login" }} />
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLInk item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLInk item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLInk item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+
+      <button className={styles.menu} onClick={() => setOpen((prev) => !prev)}>
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLInk item={link} key={link.title} />
+          ))}
+        </div>
       )}
     </div>
   );
