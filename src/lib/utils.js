66de5877a95e8d60600ae6eb0e2916abf86137e9
Shantanu993 <shantanugahtori4@gmail.com>
2024-03-24 13:06:51 +0530

    refactor db connection util

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-dotenv.config();
+const connection = {};
 
-const connectToDb = async () => {
+export const connectToDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
+    if (connection.isConnected) {
+      console.log("Using existing connection");
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGO);
+    connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error);
-    throw new Error("Unable to connect to database");
+    throw new Error(error);
   }
 };
-
-export { connectToDb };
